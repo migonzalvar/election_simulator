0dc3beb430842b034afc1c9f867ab476c9ee8556
Miguel Gonzalez <migonzalvar@gmail.com>
2014-02-10 17:14:27 +0100

    Move inside a package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='dhondt',
+    version='0.1',
+    packages=['dhondt'],
+    url='',
+    license='MIT',
+    author='migonzalvar',
+    author_email='migonzalvar@gmail.com',
+    description=''
+)
